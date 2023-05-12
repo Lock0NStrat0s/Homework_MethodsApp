fee53dd268acd7a8ad5166b9d89fb9814722571b
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 07:28:49 -0600

    code complete.

--- Homework_Methods/Program.cs
+++ Homework_Methods/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+// Create a Welcome method, a method to ask for user's name
+// and another to tell that user "Hello <name>".
+// Call these methods from program.cs
+
+using Homework_Methods;
+
+Welcome.WelcomeUser();
+string firstName = Welcome.GetFirstName();
+Welcome.GreetUser(firstName);
---
+++ Homework_Methods/Welcome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Methods;
+
+public static class Welcome
+{
+    public static void WelcomeUser()
+    {
+        Console.WriteLine("Welcome!");
+    }
+
+    public static string GetFirstName()
+    {
+        Console.Write("What is your first name: ");
+        return Console.ReadLine();
+    }
+
+    public static void GreetUser(string firstName)
+    {
+        Console.WriteLine($"Hello {firstName}.");
+    }
+}
